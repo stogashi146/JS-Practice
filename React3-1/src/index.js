cc14a9145f5ca45cc6f231bbdea2403e82cbabe7
stogashi146 <stogashi146@gmail.com>
2022-03-15 11:55:44 +0900

    React

---
+++ React3-1/src/index.js
@@ -0,0 +1,7 @@
+console.log("test")
+
+const title1 = document.getElementById("title");
+console.log(title1);
+
+const title2 = document.querySelector("#title");
+console.log(title2);
---
+++ React4-1/my-app/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+// import { App } from "./App";
+// import { CssModules } from "./CssModules";
+// import { StyledJsx } from "./StyledJsx";
+// import { Emotion } from "./Emotion";
+import { TailwindCss } from "./TailwindCss";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<CssModules />, document.getElementById("root"));
+// ReactDOM.render(<StyledJsx />, document.getElementById("root"));
+// ReactDOM.render(<Emotion />, document.getElementById("root"));
+ReactDOM.render(<TailwindCss />, document.getElementById("root"));
